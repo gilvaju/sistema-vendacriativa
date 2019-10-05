add973584f41bb7f9408b4dc0cb73937d655dedd
gilvaju <contato@gilvancosta.com>
2019-10-05 17:36:44 -0300

    styling login view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SGVC</title>
     
+    <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
-    <div>
-        Venda Criativa 1
+    <div class="container-fluid">
+        <div class="row min-vh-100 d-flex flex-column justify-content-center">
+            <div class="col-12">
+                <div class="card card-signin mx-auto">
+                    <div class="card-body">
+                        <div class="row align-items-center">
+                            <div class="col-12">
+                                <img src="img/full-logo.png" class="full-logo" alt="">
+                            </div>
+                            <div class="col-12 mt-3">
+                                <hr class="mt-1 mb-1">
+                                <input type="email" name="email" placeholder="Digite aqui seu email" class="form-control mt-3">
+                                <input type="password" name="password" placeholder="Digite aqui sua senha" class="form-control mt-3">
+                            </div>
+                            <div class="col-12">
+                                <hr>
+                                <input type="submit" value="Entrar" class="btn btn-bluevc text-light btn-block">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
