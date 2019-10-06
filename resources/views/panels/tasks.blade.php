e377e3d0010eb54dd07dbe11008c0c5965769608
gilvaju <contato@gilvancosta.com>
2019-10-06 10:36:42 -0300

    styling dasboard view

--- resources/views/panels/tasks.blade.php
+++ resources/views/panels/tasks.blade.php
@@ -1,11 +1,14 @@
-<ul>
-    <li>Teste</li>
-    <li>Teste</li>
-    <li>Teste</li>
-    <li>Teste</li>
-    <li>Teste</li>
-    <li>Teste</li>
-    <li>Teste</li>
-    <li>Teste</li>
-    <li>Teste</li>
-</ul>
+<div class="input-group">
+  <input type="text" class="form-control" name="title" id="" placeholder="Inserir tarefa">
+  <div class="input-group-append">
+    <button class="btn btn-outline-secondary bg-orangevc text-white" type="button"><i class="fas fa-plus"></i></button>
+  </div>
+</div>
+<hr>
+<div class="list-group">
+  @foreach($tasks as $task)
+    <button type="button" class="list-group-item list-group-item-action list-group-flush">
+        <span class="ml-2">{{ $task->title }}</span>
+    </button>
+  @endforeach
+</div>
