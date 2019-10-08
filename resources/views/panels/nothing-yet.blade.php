8164f5d334f9f20810e2c014219d45a30ea4969b
gilvaju <contato@gilvancosta.com>
2019-10-08 10:27:04 -0300

    fixing file name for nothing-yet view

---
+++ resources/views/panels/nothing-yet.blade.php
@@ -0,0 +1,9 @@
+<div class="row min-vh-100 d-flex flex-column justify-content-center">
+    <div class="col-12 text-center">
+        <h1 class="display-6">Ainda em <br> Desenvolvimento!</h1>
+        <a class="btn btn-primary bg-orangevc btn-block" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sair</a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
+    </div>
+</div>
--- resources/views/panels/nothing-yet.php
+++
@@ -1,5 +0,0 @@
-<div class="row min-vh-100 d-flex flex-column justify-content-center">
-    <div class="col-12 text-center">
-        <h1 class="display-6">Ainda em <br> Desenvolvimento!</h1>
-    </div>
-</div>
