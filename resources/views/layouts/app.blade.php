59b5d3f31f7309505a1ace36fd0070174fe9e647
gilvaju <contato@gilvancosta.com>
2019-10-08 18:13:01 -0300

    general adjusments and new styling for app.layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,45 @@
 <!DOCTYPE html>
-<html lang="pt">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SGVC</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    
+
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/6ecb33213d.js" crossorigin="anonymous"></script>
 </head>
-<body>
-    <div>
-        @yield('content')
+<body class="min-vh-75" style="background-color: orange">
+    <div class="container-fluid">
+        <div class="row d-flex flex-column justify-content-center bg-primary" style="height: 10vh">
+            <p>Teste</p>
+        </div>
+        <div class="row d-flex flex-column justify-content-center bg-secondary" style="height: 80vh; overflow: auto">
+            <div class="pb-3 pt-3">
+                <p class="display-1">Teste</p>
+                <p class="display-1">Teste</p>
+                <p class="display-1">Teste</p>
+                <p class="display-1">Teste</p>
+                <p class="display-1">Teste</p>
+                <p class="display-1">Teste</p>
+            </div>
+        </div>
+        <div class="row d-flex flex-column justify-content-center bg-dark" style="height: 10vh">
+            <p>Teste</p>
+        </div>
+        <!-- Code begins here -->
+        <a href="#" class="float">
+            <i class="fas fa-plus my-float fa-2x"></i>
+        </a>
     </div>
 </body>
 </html>
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SGVC</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/6ecb33213d.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div>
+        @yield('content')
+    </div>
+</body>
+</html>
