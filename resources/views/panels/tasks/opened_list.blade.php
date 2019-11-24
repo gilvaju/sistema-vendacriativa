10ea921c31cb0c79c9532a89e11c3be9374d8417
Gilvan Jr - @gilvaju <code@gilvancosta.com>
2019-11-24 19:24:06 -0300

    add action buttons for opened tasks list

--- resources/views/panels/tasks/opened_list.blade.php
+++ resources/views/panels/tasks/opened_list.blade.php
@@ -7,26 +7,68 @@
 
 @foreach($openedTasks as $task)
     <div class="list-group-item list-task-group">
-        <div class="row">
-            <div class="col-12">
+        <div class="row no-gutters">
+            <div class="col-10">
                 <h5>{{ $task->title }}</h5>
                 <p>{{ $task->description }}</p>
                 <span class="badge badge-primary p-1">{{ $task->responsible->name }}</span>
                 <span class="badge badge-secondary p-1">{{ $task->owner->name }}</span>
                 <span class="badge badge-danger p-1">{{ config('sgvc.clients.'.$task->client_id) }}</span>
             </div>
-{{--            <div class="col-4 text-right d-inline-flex my-auto mx-auto">--}}
-{{--                <button type="submit" class="btn btn-outline-light border-0 bg-white text-dark btn-sm mr-1">--}}
-{{--                    <i class="far fa-edit fa-2x"></i>--}}
-{{--                </button>--}}
-{{--                <form action="{{ route('task.destroy', $task->id) }}" method="POST">--}}
-{{--                    @csrf--}}
-{{--                    @method('DELETE')--}}
-{{--                    <button type="submit" class="btn btn-outline-light border-0 bg-white text-danger btn-sm">--}}
-{{--                        <i class="far fa-trash-alt fa-2x"></i>--}}
-{{--                    </button>--}}
-{{--                </form>--}}
-{{--            </div>--}}
+            <div class="col-2 text-right d-inline-flex my-auto mx-auto">
+                <div class="row no-gutters">
+                    <div class="col-6">
+                        <button type="submit" class="btn btn-outline-light border-0 bg-white text-dark btn-sm">
+                            <i class="far fa-edit"></i>
+                        </button>
+                    </div>
+                    <div class="col-6">
+                        <form action="{{ route('task.destroy', $task->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-outline-success border-0 bg-white text-success btn-sm">
+                                <i class="fas fa-stop-circle"></i>
+                            </button>
+                        </form>
+                    </div>
+                    <div class="col-6">
+                        <form action="{{ route('task.destroy', $task->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-outline-danger border-0 bg-white text-danger btn-sm">
+                                <i class="fas fa-stop-circle"></i>
+                            </button>
+                        </form>
+                    </div>
+                    <div class="col-6">
+                        <form action="{{ route('task.destroy', $task->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-outline-success border-0 bg-white text-success btn-sm">
+                                <i class="fas fa-check-double"></i>
+                            </button>
+                        </form>
+                    </div>
+                    <div class="col-6">
+                        <form action="{{ route('task.destroy', $task->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-outline-success border-0 bg-white text-success btn-sm">
+                                <i class="fas fa-check"></i>
+                            </button>
+                        </form>
+                    </div>
+                    <div class="col-6">
+                        <form action="{{ route('task.destroy', $task->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-outline-warning border-0 bg-white text-warning btn-sm">
+                                <i class="far fa-trash-alt"></i>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   @endforeach
