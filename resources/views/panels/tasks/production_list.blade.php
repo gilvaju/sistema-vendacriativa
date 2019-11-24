0dbb40bb415d1a2b3be8ea432b940edad0c4fd08
Gilvan Jr - @gilvaju <code@gilvancosta.com>
2019-11-24 17:38:58 -0300

    refactoring task list styling

--- resources/views/panels/tasks/production_list.blade.php
+++ resources/views/panels/tasks/production_list.blade.php
@@ -1,12 +1,12 @@
 @foreach($productionTasks as $task)
     <div class="list-group-item list-task-group todo-list">
         <div class="row">
-            <div class="col-8">
-                <h3 class="mb-0">{{ $task->title }}</h3>
-                <p>
-                    <span class="badge badge-primary p-1">Concluída</span>
-                    {{ $task->description }}
-                </p>
+            <div class="col-12">
+                <h5>{{ $task->title }}</h5>
+                <p>{{ $task->description }}</p>
+                <span class="badge badge-primary p-1">{{ $task->responsible_id }}</span>
+                <span class="badge badge-secondary p-1">{{ $task->owner_id }}</span>
+                <span class="badge badge-danger p-1">{{ config('sgvc.clients.'.$task->client_id) }}</span>
             </div>
         </div>
     </div>
