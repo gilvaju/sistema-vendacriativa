dc865fe107a5caa32dceadf223593e0d9a8cd90d
Gilvan Jr - @gilvaju <code@gilvancosta.com>
2019-11-27 17:33:59 -0300

    more styling for tasks

--- resources/views/panels/tasks/partials/badgets.blade.php
+++ resources/views/panels/tasks/partials/badgets.blade.php
@@ -1,6 +1,4 @@
 <div class="col-8 task-content-item">
-    <h5>{{ $task->title }}</h5>
-    <p>{{ $task->description }}</p>
     <span class="badge badge-primary p-1">{{ $task->responsible->name }}</span>
     <span class="badge badge-secondary p-1">{{ $task->owner->name }}</span>
     <span class="badge badge-danger p-1">{{ config('sgvc.clients.'.$task->client_id) }}</span>
--- resources/views/panels/tasks/production_list.blade.php
+++ resources/views/panels/tasks/production_list.blade.php
@@ -8,6 +8,8 @@
 @foreach($productionTasks as $task)
     <div class="list-group-item list-task-group content-task-production-list production-list">
         <div class="row no-gutters">
+            <h5>{{ $task->title }}</h5>
+            <p>{{ $task->description }}</p>
             @include('panels.tasks.partials.badgets')
             @include('panels.tasks.partials.task_actions')
         </div>
