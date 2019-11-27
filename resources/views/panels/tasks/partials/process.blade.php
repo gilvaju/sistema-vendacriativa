4af5c2697c61d993b0c128bdc64da93552fc506f
Gilvan Jr - @gilvaju <code@gilvancosta.com>
2019-11-27 19:52:27 -0300

    colorizing process

--- resources/views/panels/tasks/partials/process.blade.php
+++ resources/views/panels/tasks/partials/process.blade.php
@@ -1,13 +1,13 @@
 <div class="container-fluid mt-3 mb-2 pl-0 pr-0">
     <div class="row no-gutters d-flex justify-content-center">
-        <div class="col-1"><i class="fas fa-2x fa-step-backward text-info"></i></div>
+        <div class="col-1"><i class="fas fa-2x fa-step-backward" style="color: lightgray"></i></div>
         <div class="col-1 text-secondary d-flex align-items-center justify-content-center"><i class="fas fa-chevron-right"></i></div>
-        <div class="col-1"><i class="fab fa-2x fa-product-hunt text-secundary"></i></div>
+        <div class="col-1"><i class="fab fa-2x fa-product-hunt" style="color: lightblue"></i></div>
         <div class="col-1 text-secondary d-flex align-items-center justify-content-center"><i class="fas fa-chevron-right"></i></div>
-        <div class="col-1"><i class="fas fa-2x fa-check text-primary"></i></div>
+        <div class="col-1"><i class="fas fa-2x fa-check" style="color: lightseagreen"></i></div>
         <div class="col-1 text-secondary d-flex align-items-center justify-content-center"><i class="fas fa-chevron-right"></i></div>
-        <div class="col-1"><i class="fas fa-2x fa-check-double text-success"></i></div>
+        <div class="col-1"><i class="fas fa-2x fa-check-double" style="color: rgba(161, 211, 154, 0.6)"></i></div>
         <div class="col-1 text-secondary d-flex align-items-center justify-content-center">|</div>
-        <div class="col-1"><i class="fas fa-2x fa-stop-circle text-danger"></i></div>
+        <div class="col-1"><i class="fas fa-2x fa-stop-circle" style="color: lightsalmon"></i></div>
     </div>
 </div>
