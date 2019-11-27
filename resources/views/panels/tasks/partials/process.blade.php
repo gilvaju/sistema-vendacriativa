06ac90c7ab7be77d327397569bb5a9abe650f634
Gilvan Jr - @gilvaju <code@gilvancosta.com>
2019-11-27 19:09:06 -0300

    formating styles for process

--- resources/views/panels/tasks/partials/process.blade.php
+++ resources/views/panels/tasks/partials/process.blade.php
@@ -1,31 +1,13 @@
-<div class="container-fluid">
-    <div class="row mb-3 no-gutters d-flex justify-content-center">
-        <div class="col-1 d-inline">
-            <i class="fas fa-step-backward text-info"></i>
-        </div>
-        <div class="col-1">
-            <span><i class="fas fa-chevron-right"></i></span>
-        </div>
-        <div class="col-1">
-            <i class="fab fa-product-hunt text-secundary"></i>
-        </div>
-        <div class="col-1">
-            <span><i class="fas fa-chevron-right"></i></span>
-        </div>
-        <div class="col-1">
-            <i class="fas fa-check text-primary"></i>
-        </div>
-        <div class="col-1">
-            <span><i class="fas fa-chevron-right"></i></span>
-        </div>
-        <div class="col-1">
-            <i class="fas fa-check-double text-success"></i>
-        </div>
-        <div class="col-1">
-            <span><i class="fas fa-unlink"></i></span>
-        </div>
-        <div class="col-1">
-            <i class="fas fa-stop-circle text-danger"></i>
-        </div>
+<div class="container-fluid mt-3 mb-2">
+    <div class="row no-gutters d-flex justify-content-center">
+        <div class="col-1"><i class="fas fa-step-backward text-info"></i></div>
+        <div class="col-1"><i class="fas fa-chevron-right"></i></div>
+        <div class="col-1"><i class="fab fa-product-hunt text-secundary"></i></div>
+        <div class="col-1"><i class="fas fa-chevron-right"></i></div>
+        <div class="col-1"><i class="fas fa-check text-primary"></i></div>
+        <div class="col-1"><i class="fas fa-chevron-right"></i></div>
+        <div class="col-1"><i class="fas fa-check-double text-success"></i></div>
+        <div class="col-1"><i class="fas fa-unlink"></i></div>
+        <div class="col-1"><i class="fas fa-stop-circle text-danger"></i></div>
     </div>
 </div>
