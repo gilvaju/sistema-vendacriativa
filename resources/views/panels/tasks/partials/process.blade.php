6e4a79b47b693ef1f4d9397e2f53b67170bb46f5
Gilvan Jr - @gilvaju <code@gilvancosta.com>
2019-11-27 19:49:00 -0300

    large icons for processs

--- resources/views/panels/tasks/partials/process.blade.php
+++ resources/views/panels/tasks/partials/process.blade.php
@@ -1,13 +1,13 @@
 <div class="container-fluid mt-3 mb-2 pl-0 pr-0">
     <div class="row no-gutters d-flex justify-content-center">
-        <div class="col-1"><i class="fas fa-step-backward text-info"></i></div>
-        <div class="col-1"><i class="fas fa-chevron-right"></i></div>
-        <div class="col-1"><i class="fab fa-product-hunt text-secundary"></i></div>
-        <div class="col-1"><i class="fas fa-chevron-right"></i></div>
-        <div class="col-1"><i class="fas fa-check text-primary"></i></div>
-        <div class="col-1"><i class="fas fa-chevron-right"></i></div>
-        <div class="col-1"><i class="fas fa-check-double text-success"></i></div>
-        <div class="col-1"><i class="fas fa-unlink"></i></div>
-        <div class="col-1"><i class="fas fa-stop-circle text-danger"></i></div>
+        <div class="col-1"><i class="fas fa-2x fa-step-backward text-info"></i></div>
+        <div class="col-1 text-secondary d-flex align-items-center justify-content-center"><i class="fas fa-chevron-right"></i></div>
+        <div class="col-1"><i class="fab fa-2x fa-product-hunt text-secundary"></i></div>
+        <div class="col-1 text-secondary d-flex align-items-center justify-content-center"><i class="fas fa-chevron-right"></i></div>
+        <div class="col-1"><i class="fas fa-2x fa-check text-primary"></i></div>
+        <div class="col-1 text-secondary d-flex align-items-center justify-content-center"><i class="fas fa-chevron-right"></i></div>
+        <div class="col-1"><i class="fas fa-2x fa-check-double text-success"></i></div>
+        <div class="col-1 text-secondary d-flex align-items-center justify-content-center">|</div>
+        <div class="col-1"><i class="fas fa-2x fa-stop-circle text-danger"></i></div>
     </div>
 </div>
