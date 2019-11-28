78935c7a99ee4060b4386fdf6ba5fbb8a86bda22
Gilvan Jr - @gilvaju <code@gilvancosta.com>
2019-11-28 11:11:27 -0300

    fixing bug on delivery field

--- app/Task.php
+++ app/Task.php
@@ -8,11 +8,9 @@ class Task extends Model
 {
     protected $fillable = ['title', 'description', 'status', 'delivery_date', 'owner_id', 'responsible_id', 'client_id'];
 
-    public $delivery = null;
-
     public function delivery()
     {
-        $this->delivery = date( 'd/m/Y' , strtotime($this->delivery_date));
+        return date( 'd/m/Y' , strtotime($this->delivery_date));
     }
 
     /**
