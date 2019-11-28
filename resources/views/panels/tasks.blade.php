9635da94596b84726b7e450176a64be104591d0c
Gilvan Jr - @gilvaju <code@gilvancosta.com>
2019-11-27 21:51:22 -0300

    move some files

--- resources/views/panels/tasks.blade.php
+++
@@ -1,6 +0,0 @@
-@include('panels.tasks.partials.process')
-@include('panels.tasks.pendent_list')
-@include('panels.tasks.aprove_list')
-@include('panels.tasks.production_list')
-@include('panels.tasks.opened_list')
-@include('panels.tasks.closed_list')
---
+++ resources/views/panels/tasks/partials/tasks.blade.php
@@ -0,0 +1,6 @@
+@include('panels.tasks.partials.process')
+@include('panels.tasks.sections.pendent_list')
+@include('panels.tasks.sections.aprove_list')
+@include('panels.tasks.sections.production_list')
+@include('panels.tasks.sections.opened_list')
+@include('panels.tasks.sections.closed_list')
