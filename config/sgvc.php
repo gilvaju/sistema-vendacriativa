3bbef1ee31b08812ff7560876497422935dbd62b
Gilvan Jr - @gilvaju <code@gilvancosta.com>
2019-11-28 14:45:56 -0300

    fixing bug on delivery field

--- config/sgvc.php
+++ config/sgvc.php
@@ -8,6 +8,7 @@ return [
         '3' => 'Aprovar',
         '4' => 'Concluída',
         '5' => 'Produção',
+        '6' => 'Brifado',
     ],
 
     'clients' => [
@@ -21,6 +22,10 @@ return [
         '8' => 'Tv Futuro',
         '9' => 'O Anti Sucesso',
         '10' => 'Seu hipnoterapeuta',
+        '11' => 'Grupo Jerap',
+        '12' => 'Droff',
+        '13' => 'Matersol',
+        '14' => 'Rei do cuscuz',
     ]
 
 ];
