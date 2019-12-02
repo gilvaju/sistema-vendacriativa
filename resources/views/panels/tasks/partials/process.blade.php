45090edcde25d914c5d94c35de97dc7ccb8bc56b
Gilvan Jr - @gilvaju <code@gilvancosta.com>
2019-12-02 16:56:46 -0300

    fixing bug on title aprove group tasks and show icons for group list tasks in coluns

--- resources/views/panels/tasks/partials/process.blade.php
+++ resources/views/panels/tasks/partials/process.blade.php
@@ -1,13 +1,10 @@
 <div class="container-fluid mt-3 mb-2 pl-0 pr-0">
     <div class="row no-gutters d-flex justify-content-center">
-        <div class="col-1"><i class="fas fa-2x fa-plus text-opened"></i></div>
-        <div class="col-1 text-secondary d-flex align-items-center justify-content-center"><i class="fas fa-chevron-right"></i></div>
-        <div class="col-1"><i class="fab fa-2x fa-product-hunt text-production"></i></div>
-        <div class="col-1 text-secondary d-flex align-items-center justify-content-center"><i class="fas fa-chevron-right"></i></div>
-        <div class="col-1"><i class="fas fa-2x fa-check text-aprove"></i></div>
-        <div class="col-1 text-secondary d-flex align-items-center justify-content-center"><i class="fas fa-chevron-right"></i></div>
-        <div class="col-1"><i class="fas fa-2x fa-check-double text-closed"161, 211, 154, 0.6)"></i></div>
-        <div class="col-1 text-secondary d-flex align-items-center justify-content-center">|</div>
-        <div class="col-1"><i class="fas fa-2x fa-stop-circle text-pendent"></i></div>
+        <div class="col-1"><i class="fas fa-2x fa-plus text-opened"></i><span><i class="fas fa-chevron-right"></i></span></div>
+        <div class="col-1"><i class="fas fa-2x fa-list"></i><span><i class="fas fa-chevron-right"></i></span></div>
+        <div class="col-1"><i class="fas fa-2x fa-edit text-production"></i><span><i class="fas fa-chevron-right"></i></span></div>
+        <div class="col-1"><i class="fas fa-2x fa-check text-aprove"></i><span><i class="fas fa-chevron-right"></i></span></div>
+        <div class="col-1"><i class="fas fa-2x fa-stop-circle text-pendent"></i><span><i class="fas fa-chevron-right"></i></span></div>
+        <div class="col-1"><i class="fas fa-2x fa-check-double text-closed"></i></div>
     </div>
 </div>
--- resources/views/panels/tasks/partials/title_task.blade.php
+++ resources/views/panels/tasks/partials/title_task.blade.php
@@ -1,6 +1,25 @@
 <div class="row">
     <div class="col-12">
-        <h5>{{ $statusGroupTasks}} | {{ $countTasks }}</h5>
+            @switch($statusGroupTasks)
+                @case('Abertas')
+                    <h5><i class="fas fa-plus text-opened"></i> {{ $statusGroupTasks}} | {{ $countTasks }}</h5>
+                    @break
+                @case('Brifadas')
+                    <h5><i class="fas fa-list"></i> {{ $statusGroupTasks}} | {{ $countTasks }}</h5>
+                    @break
+                @case('Produzindo')
+                    <h5><i class="fas fa-edit text-production"></i> {{ $statusGroupTasks}} | {{ $countTasks }}</h5>
+                    @break
+                @case('Aprovar')
+                    <h5><i class="fas fa-check text-aprove"></i> {{ $statusGroupTasks}} | {{ $countTasks }}</h5>
+                    @break
+                @case('Pendentes')
+                    <h5><i class="fas fa-stop-circle text-pendent"></i> {{ $statusGroupTasks}} | {{ $countTasks }}</h5>
+                    @break
+                @case('Concluídas')
+                    <h5><i class="fas fa-check-double text-closed"></i> {{ $statusGroupTasks}} | {{ $countTasks }}</h5>
+                    @break
+        @endswitch
         <hr>
     </div>
 </div>
