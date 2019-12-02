7a5bd9846b90621d80a5f7c55a886e631aa00070
Gilvan Jr - @gilvaju <code@gilvancosta.com>
2019-12-02 17:02:57 -0300

    alter order for status on mobile and desktop

--- resources/views/panels/tasks/partials/tasks.blade.php
+++ resources/views/panels/tasks/partials/tasks.blade.php
@@ -1,20 +1,41 @@
-{{--@include('panels.tasks.partials.process')--}}
-<div class="col-12 col-xl-2">
-    @include('panels.tasks.sections.opened_list')
-</div>
-<div class="col-12 col-xl-2">
-    @include('panels.tasks.sections.brified_list')
-</div>
-<div class="col-12 col-xl-2">
-    @include('panels.tasks.sections.production_list')
-</div>
-<div class="col-12 col-xl-2">
-    @include('panels.tasks.sections.aprove_list')
-</div>
-<div class="col-12 col-xl-2">
-    @include('panels.tasks.sections.pendent_list')
-</div>
-{{--<div class="col-12 d-none d-sm-block d-md-none">--}}
-<div class="col-12 col-xl-2">
-    @include('panels.tasks.sections.closed_list')
-</div>
+@if ($agent->isMobile())
+    <div class="col-12 col-xl-2">
+        @include('panels.tasks.sections.pendent_list')
+    </div>
+    <div class="col-12 col-xl-2">
+        @include('panels.tasks.sections.aprove_list')
+    </div>
+    <div class="col-12 col-xl-2">
+        @include('panels.tasks.sections.production_list')
+    </div>
+    <div class="col-12 col-xl-2">
+        @include('panels.tasks.sections.brified_list')
+    </div>
+    <div class="col-12 col-xl-2">
+        @include('panels.tasks.sections.opened_list')
+    </div>
+    {{--<div class="col-12 d-none d-sm-block d-md-none">--}}
+    <div class="col-12 col-xl-2">
+        @include('panels.tasks.sections.closed_list')
+    </div>
+    @else
+        <div class="col-12 col-xl-2">
+            @include('panels.tasks.sections.opened_list')
+        </div>
+        <div class="col-12 col-xl-2">
+            @include('panels.tasks.sections.brified_list')
+        </div>
+        <div class="col-12 col-xl-2">
+            @include('panels.tasks.sections.production_list')
+        </div>
+        <div class="col-12 col-xl-2">
+            @include('panels.tasks.sections.aprove_list')
+        </div>
+        <div class="col-12 col-xl-2">
+            @include('panels.tasks.sections.pendent_list')
+        </div>
+        {{--<div class="col-12 d-none d-sm-block d-md-none">--}}
+        <div class="col-12 col-xl-2">
+            @include('panels.tasks.sections.closed_list')
+        </div>
+@endif
