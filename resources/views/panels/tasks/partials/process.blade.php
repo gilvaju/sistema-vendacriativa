90eaef954fabd33fe5bc33ca3680c84ba765603c
Gilvan Jr - @gilvaju <code@gilvancosta.com>
2019-12-03 17:53:17 -0300

    add more styles for process

--- resources/views/panels/tasks/partials/process.blade.php
+++ resources/views/panels/tasks/partials/process.blade.php
@@ -1,11 +1,31 @@
 <div class="container-fluid mt-3 mb-2 pl-0 pr-0">
-    <div class="row no-gutters d-flex justify-content-center">
-        <div class="col-1"><i class="fas fa-2x fa-plus text-opened"></i><span><i class="fas fa-chevron-right"></i></span></div>
-        <div class="col-1"><i class="fas fa-2x fa-list text-briefed"></i><span><i class="fas fa-chevron-right"></i></span></div>
-        <div class="col-1"><i class="fas fa-2x fa-pause-circle text-paused"></i><span><i class="fas fa-chevron-right"></i></span></div>
-        <div class="col-1"><i class="fas fa-2x fa-edit text-production"></i><span><i class="fas fa-chevron-right"></i></span></div>
-        <div class="col-1"><i class="fas fa-2x fa-stop-circle text-pendent"></i><span><i class="fas fa-chevron-right"></i></span></div>
-        <div class="col-1"><i class="fas fa-2x fa-check text-aprove"></i><span><i class="fas fa-chevron-right"></i></span></div>
-        <div class="col-1"><i class="fas fa-2x fa-check-double text-closed"></i></div>
+    <div class="row no-gutters d-flex justify-content-center process align-items-center">
+        <div class="col-1 d-flex justify-content-between align-items-center">
+            <i class="fas fa-2x fa-fw fa-plus text-opened"></i>
+        </div>
+        <i class="fas fa-fw fa-chevron-right"></i>
+        <div class="col-1 d-flex justify-content-between align-items-center">
+            <i class="fas fa-2x fa-fw fa-list text-briefed"></i>
+        </div>
+        <i class="fas fa-fw fa-chevron-right"></i>
+        <div class="col-1 d-flex justify-content-between align-items-center">
+            <i class="fas fa-2x fa-fw fa-pause-circle text-paused"></i>
+        </div>
+        <i class="fas fa-fw fa-chevron-right"></i>
+        <div class="col-1 d-flex justify-content-between align-items-center">
+            <i class="fas fa-2x fa-fw fa-edit text-production"></i>
+        </div>
+        <i class="fas fa-fw fa-chevron-right"></i>
+        <div class="col-1 d-flex justify-content-between align-items-center">
+            <i class="fas fa-2x fa-fw fa-stop-circle text-pendent"></i>
+        </div>
+        <i class="fas fa-fw fa-chevron-right"></i>
+        <div class="col-1 d-flex justify-content-between align-items-center">
+            <i class="fas fa-2x fa-fw fa-check text-aprove"></i>
+        </div>
+        <i class="fas fa-fw fa-chevron-right"></i>
+        <div class="col-1 d-flex justify-content-between align-items-center">
+            <i class="fas fa-2x fa-fw fa-check-double text-closed"></i>
+        </div>
     </div>
 </div>
